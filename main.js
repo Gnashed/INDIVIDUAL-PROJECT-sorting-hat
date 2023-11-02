68af1aa9c490ad2f142dec830830e298013bf05d
Tion Blackmon <tionblackmon@icloud.com>
2023-11-02 18:41:25 -0500

    Got "Start Here" button centered.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // TODO: Create Data structure(s), then create functions to manipulate it.
 
 // Student array of objects should have a house color, name, house, and isExpelled initialized to false.
-const Students = [
+const students = [
     {
         id: 1,
-        name: "Tion Blackmon",
-        house: "Ravenclaw",
-        houseColor: "Blue",
+        name: "Student Name",
+        house: "House Name",
+        houseColor: "House Color",
         isExpelled: false
     }
 ];
