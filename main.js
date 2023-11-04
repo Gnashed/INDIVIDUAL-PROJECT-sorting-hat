ec4e3d3aebe231bd8d5a3bf12d57c27ec5351917
Tion Blackmon <tionblackmon@icloud.com>
2023-11-04 07:52:08 -0500

    Removed commented code.

--- main.js
+++ main.js
@@ -65,13 +65,6 @@ const sortStudent = (event) => {
     document.querySelector('form').reset();
 }
 
-// Produces a random number from houses array, then use the index to assign the student to a house.
-// const assignToHouse = () => {
-//     let assignedHouseIndex = Math.floor(Math.random() * houses.length);
-//     students.house = houses[assignedHouseIndex];
-//     // console.log(students.house);
-// };
-
 /*
 ========================================= main() =========================================
 Will revisit this when I'm finished building the other functions. Basically the same as startApp(). Just naming it differently.
