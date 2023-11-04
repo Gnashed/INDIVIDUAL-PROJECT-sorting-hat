f579edff81ac7b5e3b4f0fc46139279882ae659d
Tion Blackmon <tionblackmon@icloud.com>
2023-11-03 19:16:32 -0500

    Rearranged function orders since it was bugging me.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// TODO: Create Data structure(s), then create functions to manipulate it.
-
-// Student array of objects should have a house color, name, house, and isExpelled initialized to false.
+/*
+========================================= Arrays =========================================
+*/
 const students = [
     {
         id: 1,
@@ -13,42 +13,6 @@ const students = [
 
 // Might need to move this array into a function later...
 const houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
-
-// Produces a random number from houses array, then use the index to assign the student to a house.
-const assignToHouse = (student) => {
-    let assignedHouseIndex = Math.floor(Math.random() * houses.length);
-    students.house = houses[assignedHouseIndex];
-    // console.log(students.house);
-};
-
-/*
-========================================= Form =========================================
-*/
-// Target input textbox
-const form = document.querySelector('form');
-
-// Create student and add it to first year's.
-const sortStudent = (event) => {
-    event.preventDefault();
-
-    // New Student
-    const newStudent = {
-        id: students.length + 1,
-        name: document.querySelector("#student").value,
-        house: "Placeholder", // Will figure out later.
-        houseColor: "Placeholder", // Will figure out later.
-        isExpelled: false
-    }
-
-    // Push student to the students array
-    students.push(newStudent);
-    // Render students to the DOM.
-    renderCards(students);
-    // Reset the form.
-    form.reset();
-}
-
-form.addEventListener('click', sortStudent);
 
 /*
 ========================================= Cards =========================================
@@ -78,10 +42,43 @@ const renderCards = (array) => {
 }
 
 /*
+========================================= Form =========================================
+*/
+// Target form
+const form = document.querySelector('form');
+
+// Create student and add it to first year's.
+const sortStudent = (event) => {
+    event.preventDefault();
+
+    // New Student
+    const newStudent = {
+        id: students.length + 1,
+        name: document.querySelector("#student").value,
+        house: "Placeholder", // Will figure out later.
+        houseColor: "Placeholder", // Will figure out later.
+        isExpelled: false
+    }
+
+    // Push student to the students array
+    students.push(newStudent);
+    // Render students to the DOM.
+    renderCards(students);
+    // Reset the form.
+    form.reset();
+}
+
+// Produces a random number from houses array, then use the index to assign the student to a house.
+const assignToHouse = (student) => {
+    let assignedHouseIndex = Math.floor(Math.random() * houses.length);
+    students.house = houses[assignedHouseIndex];
+    // console.log(students.house);
+};
+
+/*
 ========================================= main() =========================================
 Will revisit this when I'm finished building the other functions. Basically the same as startApp(). Just naming it differently.
 */
 
 // Func calls
-renderCards(students);
-// sortStudent();
+form.addEventListener('click', sortStudent);
