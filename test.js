1d422cb218c72f105b4ef710be7fe60f47a67e20
Tion Blackmon <tionblackmon@icloud.com>
2023-11-09 23:22:15 -0600

    test.js is temporary. Practicing destructuring to understand the concept.

---
+++ test.js
@@ -0,0 +1,19 @@
+const fourLeggedPets = [
+    {
+        animal: "Dog",
+        avgLifeSpanYears: 13,
+        aGoodGirlOrBoy: true,
+        favFoods: {
+            fav: "Any meat",
+            secondFav: "anything"
+        }
+    },
+
+    {
+        animal: "Cat",
+        avgLifeSpanYears: 15,
+        aGoodGirlOrBoy: false,
+        reason: "They're built different."
+    }
+
+];
